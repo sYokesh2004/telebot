e4f41283d725cdc8d2dbb824712a26dfda8fc1ea
yokesh s <146423519+sYokesh2004@users.noreply.github.com>
2023-12-14 18:25:08 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,56 @@
+import os
+import logging
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler, PicklePersistence
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Set your bot token and bot username
+TOKEN = 'YOUR_BOT_TOKEN'
+BOT_USERNAME = 'YOUR_BOT_USERNAME'
+
+def start(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi! I can help you find the movie you want to watch.')
+
+def help_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Just send me the name of the movie you want to watch and I will find it for you.')
+
+def search_movie(update: Update, context: CallbackContext) -> None:
+    """Search for a movie by its name."""
+    # Add your own code to search for the movie using an API like TMDB or IMDB.
+    # You can also use web scraping libraries like BeautifulSoup or Scrapy.
+
+    # Once you have the movie data, you can send it to the user.
+    update.message.reply_text('Here is the movie you were looking for: [Title, Year, Genre, etc.]')
+
+def main() -> None:
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks.
+    updater = Updater(TOKEN, use_context=True)
+
+    # Get the dispatcher to register handlers.
+    dp = updater.dispatcher
+
+    # Register the command handlers.
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help_command))
+
+    # Register the message handler.
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, search_movie))
+
+    # Start the Bot.
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
